a16fca0fd51ab6e5f7004bfabfd039f89643465d
VictorYsc <victorysc0617@gmail.com>
2024-08-26 16:14:37 +0800

    up

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,33 @@
-const hamMenu = document.querySelector('.ham-menu');
+const hamMenu = document.querySelector('.dropdown-menu');
 
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active')
+    ;
 });
+
+// /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
+  
+//   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+//   }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
