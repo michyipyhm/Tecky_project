16af833911566fdc86c2edb650cb7e8086786a80
michyipyhm <micmicyip@hotmail.com>
2024-08-28 17:59:03 +0800

    search

--- public/js/main.js
+++ public/js/main.js
@@ -24,14 +24,14 @@ window.onload = async () => {
 
 
 
-// function openNav() {
-//   document.getElementById("mySidenav").style.width = "250px";
-//   document.getElementById("main").style.marginLeft = "250px";
-//   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-// }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-//   document.getElementById("main").style.marginLeft = "0";
-//   document.body.style.backgroundColor = "white";
-// }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#08192c";
+}
