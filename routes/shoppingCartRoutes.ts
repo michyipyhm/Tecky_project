442453f6128996d3403e821d7cd1a56eb4e694bb
ＭＡＫＴＳＺＫＥＩ <mtszkei@gmail.com>
2024-09-09 09:56:14 +0800

    payment css, quantity

--- routes/shoppingCartRoutes.ts
+++ routes/shoppingCartRoutes.ts
@@ -6,13 +6,18 @@ export const shoppingCartRouter = express.Router()
 shoppingCartRouter.get("/shoppingcart", async (req, res) =>{
     const userId = req.session.userId;
     if (!userId) {
-        res.status(401).json({ message: "Please login first." });
+        res.status(401).json();
         return;
     }
     try {
-        let queryResult = await pgClient.query(`select * from shopping_cart join product on product.id = shopping_cart.product_id join product_image on product_image.product_id = product.id where member_id =${userId};`)
+        let queryResult = await pgClient.query(`select * from shopping_cart 
+            join product on product.id = shopping_cart.product_id 
+            join product_image on product_image.product_id = product.id 
+            join origin on origin.id = product.origin_id 
+            join format on format.id = product.format_id
+            where member_id =${userId};`)
         let data = queryResult.rows
-        console.log(data)
+        // console.log(data)
         let totalPriceQueryResult = await pgClient.query(`select sum(product_price * quantity) as total from shopping_cart 
 join product on product.id = shopping_cart.product_id   
 where member_id =${userId}; `)
@@ -25,15 +30,22 @@ where member_id =${userId}; `)
 })
 
 shoppingCartRouter.post('/selectedQuantity', async (req, res) =>{
+    try {
     let data = req.body
     let id = data.id
     let quantity = data.quantity
-    console.log(data)
-    try {
+    console.log("Product", id , "'s quantity has been changed to", quantity)
+    // 檢查stock
+    const checkStockQuery = await pgClient.query(
+        `SELECT product_quantity FROM product WHERE id = $1`,[id])
+      const currentStock = checkStockQuery.rows[0].product_quantity
+      if (currentStock < quantity) {
+        return res.status(400).json({ message: `Available: ${currentStock}, Requested: ${quantity}` });
+      }
         await pgClient.query(`UPDATE shopping_cart SET quantity = '${quantity}' WHERE product_id = '${id}';`)
-    res.status(200).json({ message: 'Quantity updated!' });
+        return res.status(200).json({ message: 'Quantity updated!' });
     } catch (err) {
       console.error(err);
-      res.status(500).send('Quantity failed');
+      return res.status(500).send('Quantity failed');
     }
 })
